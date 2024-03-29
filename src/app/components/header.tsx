07d2af5e4fc01eb792a8625aab1d07083b4ed98e
gureenkov56 <gureenkov56@gmail.com>
2024-03-30 02:35:34 +0500

    Header fix

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,12 +1,27 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import locationIcon from 'public/images/icons/location.svg'
+import logo from 'public/images/logo.png'
+
+/**
+ * TODO: 
+ * Mobile adaptive
+ * Separate styles from global.scss
+ * Location functional
+ * Search functional
+ */
 
 const Header = () => {
   const siteName = 'Abito';
 
   return(
     <header>
-      <div className="logo">{siteName}</div>
+      <div className="logo">
+        <Link href='/'>
+          <Image src={logo} alt="logo" width={30}/>
+          <span>{siteName}</span>
+        </Link>
+      </div>
       <div className="search">
         <form>
           <input type="text" />
